0f85095bf3726801f49f2fe783d9d6f65dc3baba
Akhil <akhilratna234@gmail.com>
2025-11-06 19:03:09 +0530

    version1.5

---
+++ OneDrive/Desktop/java/MultiThreading/Threading/src/MultiThreading/priorities.java
@@ -0,0 +1,26 @@
+public class priorities extends Thread {
+
+    public void run() {
+        System.out.println("Thread Name: " + Thread.currentThread().getName() + " | Priority: " + Thread.currentThread().getPriority());
+    }
+
+    public static void main(String[] args) {
+        priorities t1 = new priorities();
+        priorities t2 = new priorities();
+        priorities t3 = new priorities();
+
+        t1.setPriority(Thread.MIN_PRIORITY); // 1
+        t2.setPriority(Thread.NORM_PRIORITY); // 5
+        t3.setPriority(Thread.MAX_PRIORITY); // 10
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        System.out.println("Main Thread Priority: " + Thread.currentThread().getPriority());
+        System.out.println("Thread 1 Priority: " + t1.getPriority());
+        System.out.println("Thread 2 Priority: " + t2.getPriority());
+        System.out.println("Thread 3 Priority: " + t3.getPriority());
+    }
+    
+}
